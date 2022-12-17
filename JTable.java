cbe823d3b0e8f31b1ed9f6c5b6c633b956bc7810
JustinElroy <120807308+JustinElroy@users.noreply.github.com>
2022-12-17 21:02:46 +0400

    Add files via upload

---
+++ JTable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jtable;
+/**
+ *
+ * @author hp
+ */
+public class JTable {
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
